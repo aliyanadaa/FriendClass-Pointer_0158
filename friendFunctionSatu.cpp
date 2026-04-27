8ba3787416f9eb238cb3bfbd61fb82ec662122bc
aliyanadaa <aliyanadaq@gmail.com>
2026-04-27 17:26:10 +0700

    menambahkan class mahasiswa dan void setnama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -2,3 +2,17 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+    private:
+    string nama;
+    public:
+    friend void setnama(mahasiswa &a, string);
+};
+
+void setnama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
