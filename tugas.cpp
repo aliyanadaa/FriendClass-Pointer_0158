b21373177c2140b3f717d3d34a815b2a67818e99
aliyanadaa <aliyanadaq@gmail.com>
2026-04-27 19:56:41 +0700

    menambahkan method inputData pada BelahKetupat

--- tugas.cpp
+++ tugas.cpp
@@ -32,6 +32,19 @@ class LayangLayang
 
 class BelahKetupat
 {
-private:
-    double d1, d2, s;
-}
+    private:
+        double d1, d2, s;
+    
+    public:
+    void inputData()
+    {
+        cout << "\nInput Belah Ketupat" << endl;
+        cout << "Masukkan sisi: ";
+        cin >> s;
+        cout << "Masukkan diagonal 1 : ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2 : ";
+        cin >> d2;
+    }
+
+};
